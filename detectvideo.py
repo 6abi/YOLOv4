ba7c4f164c9dc04b133a1d56bbe86b322f417887
Bárbara <6abi.cardoso@gmail.com>
2021-05-26 00:33:18 -0300

    Adiciona metodo detecção video

--- detectvideo.py
+++ detectvideo.py
@@ -20,11 +20,11 @@ flags.DEFINE_string('weights', './checkpoints/yolov4-416',
 flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('video', 'https://www.youtube.com/watch?v=lDCs1rdYB_o', 'path to input video')
+flags.DEFINE_string('video', './data/road.mp4', 'path to input video')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.25, 'score threshold')
 flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_string('output_format', 'avi', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
 flags.DEFINE_boolean('dis_cv2_window', False, 'disable cv2 window during the process') # this is good for the .ipynb
 
 def main(_argv):
